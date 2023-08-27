32925fa69df38bc78f8c542477499d11346b78d5
Priyanshu <2003priyanshusingh@gmail.com>
2023-08-27 13:53:13 +0530

    text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,19 +23,19 @@ def main ():
     </div>
     """
     st.markdown (html_temp, unsafe_allow_html=True)
-    CRIM = st.text_input ("CRIM","Type Here")
-    ZN = st.text_input("ZN", "Type Here")
-    INDUS = st.text_input ("INDUS","Type Here")
-    CHAS = st.text_input ("CHAS", "Type Here")
-    NOX = st.text_input ("NOX", "Type Here")
-    RM = st.text_input ("RM", "Type Here")
-    Age = st.text_input ("Age", "Type Here")
-    DIS = st.text_input ("DIS", "Type Here")
-    RAD = st.text_input ("RAD", "Type Here")
-    TAX = st.text_input ("TAX", "Type Here")
-    PTRATIO = st.text_input ("PTRATIO", "Type Here")
-    B = st.text_input ("B", "Type Here")
-    LSTAT = st.text_input ("LSTAT", "Type Here")
+    CRIM = st.text_input ("CRIM","FType Here")
+    ZN = st.text_input("ZN", )
+    INDUS = st.text_input ("INDUS",)
+    CHAS = st.text_input ("CHAS", )
+    NOX = st.text_input ("NOX", )
+    RM = st.text_input ("RM", )
+    Age = st.text_input ("Age", )
+    DIS = st.text_input ("DIS", )
+    RAD = st.text_input ("RAD", )
+    TAX = st.text_input ("TAX", )
+    PTRATIO = st.text_input ("PTRATIO", )
+    B = st.text_input ("B", )
+    LSTAT = st.text_input ("LSTAT", )
     # if len(X) == 0:
     #     raise ValueError("The array passed to the `predict()` method must have at least one sample.")
 
@@ -48,7 +48,7 @@ def main ():
         result=prediction_price(CRIM,ZN,INDUS,CHAS,NOX,RM,Age,DIS,RAD,TAX,PTRATIO,B,LSTAT)
     st.success('The Price is {}'.format(result))
     if st.button("About"):
-        st.text("Lets LEarn")
+        st.text("Lets Learn")
         st.text("Built with Streamlit")
         
 if __name__ == '__main__':
